93f0af68064f61701363f8f6abfcfe9f5d54a73b
tanvi <goyaltanvi94@gmail.com>
2020-05-22 11:25:54 +0530

    loops and ranges in Kotlin

--- app/src/main/java/com/example/kotlinfundamentals/MainActivity.kt
+++ app/src/main/java/com/example/kotlinfundamentals/MainActivity.kt
@@ -1,9 +1,7 @@
 package com.example.kotlinfundamentals
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import java.lang.Integer.parseInt
-import java.lang.Long.parseLong
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,11 +13,62 @@ class MainActivity : AppCompatActivity() {
         operators()
         stringTemplates()
         conditionalExpressions()
+        loops()
         nullability()
     }
 
+    private fun loops() {
+
+        val arrayList = arrayListOf<String>("a", "b", "c")
+        for (element in arrayList) {
+            print("$element ")
+        }
+
+        println()
+
+        for ((index, element) in arrayList.withIndex()) {
+            println("$index is $element")
+        }
+
+        for (index in 0 until arrayList.size) {
+            print(arrayList[index] + ", ")
+        }
+
+        println()
+
+        for (index in arrayList.size - 1 downTo 0) {
+            print(arrayList[index] + ", ")
+        }
+
+        println()
+
+        for (index in 0 until arrayList.size step 2) {
+            print(arrayList[index] + ", ")
+        }
+
+        println()
+
+        for (element in 'd'..'m') {
+            print("$element ")
+        }
+
+        println()
+
+        var x = 0
+        while (x < arrayList.size) {
+            print("$arrayList[x] ")
+            x++;
+        }
+
+        println()
+
+        repeat(5) { i ->
+            println("We are on the ${i + 1}. loop iteration")
+        }
+    }
+
     private fun variables() {
-        val a : Int = 0 // immediate assignment by explicitly declaring type
+        val a: Int = 0 // immediate assignment by explicitly declaring type
         val b = 0 // type inference
         var c = 10
 
@@ -50,25 +99,25 @@ class MainActivity : AppCompatActivity() {
         fun isNoon(hourOfDay: Int) = if (hourOfDay >= 12) true else false // if as expression
 
         val type = 12
-        when(type) { // properties of when
+        when (type) { // properties of when
             0 -> print("Type is 0")
-            in 1 .. 10 -> println("Between 1 to 10")
-            !in 10 .. 20 -> println("Not in 10 to 20")
+            in 1..10 -> println("Between 1 to 10")
+            !in 10..20 -> println("Not in 10 to 20")
             is Int -> println("Type is of Int Type")
             else -> println("Greater than 10")
         }
     }
 
     private fun nullability() {
-        val hourEx : String = "3 hours"
-        var minsEx : String? = "15 mins"
+        val hourEx: String = "3 hours"
+        var minsEx: String? = "15 mins"
 
 //        hourEx = null // cannot assign null to it
-        minsEx = null // can assign null
+//        minsEx = null // can assign null
 
-        print(hourEx.length) // would be called safely
-        print(minsEx?.length)
-        print(minsEx?.length ?: 0)
+        println(hourEx.length) // would be called safely
+        println(minsEx?.length)
+        println(minsEx?.length ?: 0)
         println(minsEx?.length!!.dec())
     }
 }
